949a94ddc891c6c1314b05b053ba8b637c39a820
Effiong Benjamin <benjamineffiong124@yahoo.com>
2019-12-11 15:04:06 +0100

    Updates

---
+++ precache-manifest.763e7ccb3c52fa181cf5f4b5011465a4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84b8e294ddc02986ab58fad3f1ae6e82",
+    "url": "/bloomwebagency/index.html"
+  },
+  {
+    "revision": "a88c397f55c1ca3ee32d",
+    "url": "/bloomwebagency/static/css/2.5bc09813.chunk.css"
+  },
+  {
+    "revision": "51f4215a3ae9b5cf3cb5",
+    "url": "/bloomwebagency/static/css/main.1ea065f3.chunk.css"
+  },
+  {
+    "revision": "a88c397f55c1ca3ee32d",
+    "url": "/bloomwebagency/static/js/2.fef60eee.chunk.js"
+  },
+  {
+    "revision": "51f4215a3ae9b5cf3cb5",
+    "url": "/bloomwebagency/static/js/main.9ab42918.chunk.js"
+  },
+  {
+    "revision": "46e04d8f3d0d41b9dc19",
+    "url": "/bloomwebagency/static/js/runtime-main.82cd9086.js"
+  },
+  {
+    "revision": "84fb3e708ceacb7e7b4e47bcdedb78cb",
+    "url": "/bloomwebagency/static/media/BWA_logo.84fb3e70.jpg"
+  },
+  {
+    "revision": "2fd02ba669dd52d4f9d14ae631bf813e",
+    "url": "/bloomwebagency/static/media/portitem1.2fd02ba6.jpeg"
+  },
+  {
+    "revision": "4aa59e645618fac3c4d3b2b6828ffc4f",
+    "url": "/bloomwebagency/static/media/portitem2.4aa59e64.jpeg"
+  },
+  {
+    "revision": "1f639e1e606c30a9279a125f3ca74b59",
+    "url": "/bloomwebagency/static/media/portitem3.1f639e1e.jpeg"
+  },
+  {
+    "revision": "f89a13d631d052a26dfc8d5442bf5824",
+    "url": "/bloomwebagency/static/media/profile.f89a13d6.jpeg"
+  },
+  {
+    "revision": "0852ab0015d43f2e6540bf0fc82864ff",
+    "url": "/bloomwebagency/static/media/sitebg.0852ab00.jpg"
+  }
+]);
